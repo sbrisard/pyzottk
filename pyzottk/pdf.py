4e72983c4a3ed1e48830e2a6644c8b35f21546c9
Sebastien Brisard <sebastien.brisard@m4x.org>
2018-05-01 07:13:39 +0200

    Formatting

--- pyzottk/pdf.py
+++ pyzottk/pdf.py
@@ -37,10 +37,10 @@ def add_metadata(istream, ostream, author, title):
     """Add author and title metadata to PDF file.
 
     Args:
-        istream: the input PDF (string or stream in 'rb' mode)
-        ostream: the output PDF (string or stream in 'wb' mode)
-        author: the '/Author' metadata (string)
-        title: the '/Title' metadata (string)
+        istream: The input PDF (string or stream in 'rb' mode).
+        ostream: The output PDF (string or stream in 'wb' mode).
+        author: The '/Author' metadata (string).
+        title: The '/Title' metadata (string).
     """
     reader = PyPDF2.PdfFileReader(istream)
     writer = PyPDF2.PdfFileWriter()
