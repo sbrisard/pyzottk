41ec95655c23a7e4c5aaa1debdd83774cba1a2ca
Sebastien Brisard <sebastien.brisard@m4x.org>
2019-02-08 20:46:05 +0100

    Handle the `limit` parameter

--- scripts/export_collection.py
+++ scripts/export_collection.py
@@ -7,6 +7,7 @@ destination. The metadata (author and title) of the exported PDF file
 are updated according to the item data.
 """
 import configparser
+import itertools
 import os
 import sys
 
@@ -25,6 +26,7 @@ COLLECTION_HELP = 'name of collection to export'
 
 EXPORT_PATH_HELP = 'full path to export directory'
 
+ITEMS_PER_REQUEST = 10
 
 def parse_config():
     """Return the contents of the pyzottk configuration file.
@@ -62,25 +64,52 @@ def full_name(first_name, last_name):
     out += last_name
     return out
 
+
+def get_collections(user_prefix, params, proxies):
+    url = '/'.join([user_prefix, 'collections'])
+    collections = []
+    for i in itertools.count(0):
+        r = requests.get(url=url, params=params, proxies=proxies)
+        r_json = r.json()
+        collections += r_json
+        if len(r_json) < ITEMS_PER_REQUEST:
+            params['start'] = 0
+            return collections
+        params['start'] += ITEMS_PER_REQUEST
+
+
 if __name__ == '__main__':
     args = setup_argument_parser().parse_args()
 
     cfg = parse_config()
     user_prefix = '/'.join([BASE_URL, 'users', cfg['credentials']['user_id']])
-    params = {'v': 3, 'key': cfg['credentials']['key'], 'format': 'json'}
+    params = {'v': 3,
+              'key': cfg['credentials']['key'],
+              'format': 'json',
+              'limit': ITEMS_PER_REQUEST,
+              'start': 0}
     proxies = dict(cfg['proxies'])
 
     # Find key of exported collection
     url = '/'.join([user_prefix, 'collections'])
-    r = requests.get(url=url, params=params, proxies=proxies)
+    collections = get_collections(user_prefix, params, proxies)
     collection_key = None
-    for item in r.json():
-        data = item['data']
-        if data['name'] == args.collection:
-            collection_key = data['key']
+    params['start'] = 0
+
+    while collection_key is None:
+        r = requests.get(url=url, params=params, proxies=proxies)
+        collections = r.json()
+        for collection in collections:
+            data = collection['data']
+            if data['name'] == args.collection:
+                collection_key = data['key']
+                break
+        params['start'] += ITEMS_PER_REQUEST
+        if len(collections) < ITEMS_PER_REQUEST:
             break
-    else:
+    if collection_key is None:
         raise RuntimeError('could not find collection: '+args.collection)
+
 
     if args.output is None:
         args.output = os.path.join('.', args.collection)
@@ -89,10 +118,17 @@ if __name__ == '__main__':
 
     # List items in collection
     url = '/'.join([user_prefix, 'collections', collection_key, 'items/top'])
-    items = requests.get(url=url, params=params, proxies=proxies)
+    params['start'] = 0
 
-    # Export items
-    items = list(items.json())
+    items = []
+    while True:
+        r = requests.get(url=url, params=params, proxies=proxies)
+        new_items = list(r.json())
+        items += new_items
+        params['start'] += ITEMS_PER_REQUEST
+        if len(new_items) < ITEMS_PER_REQUEST:
+            break
+
     for i, item in enumerate(items):
         data = item['data']
         title = data['title']
